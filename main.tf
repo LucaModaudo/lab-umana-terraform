c88faaaf0990d3507e43e420c02413e637463e5c
LucaModaudo <91148944+LucaModaudo@users.noreply.github.com>
2024-11-28 16:16:17 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "google" {
+  project     = "ennova-apigee-x"
+  region      = "europe-west8"
+
+}
+
+resource "google_compute_network" "vpc1" {
+  name                    = "my-custom-network-1"
+  auto_create_subnetworks = "false"
+
+}
+
+resource "google_compute_subnetwork" "my-custom-subnet1" {
+  name          = "my-custom-subnet-1"
+  ip_cidr_range = "10.255.196.0/24"
+  network       = google_compute_network.vpc1.name
+  region        = "us-east1"
+}
